53fa51f2938f6eef506fee5fc6453f45e04ec12a
wafeishushu <a153255005@163.com>
2023-10-15 20:54:17 +0800

    add dojo blog

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,22 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/create" element={<Create/>}></Route>
+          <Route path="/blogs/:id" element={<BlogDetails/>}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
